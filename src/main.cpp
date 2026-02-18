667cfb4f2436d1e9026303a32d2baedb090fac8f
NastasiSantino <snastasi@itba.edu.ar>
2026-02-18 15:14:21 -0300

    Compute Angles Added. Recives anntena position and rocket position and computes the target angles.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,92 @@
 #include <Arduino.h>
+#include <math.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// =======================
+// Types / Structs
+// =======================
+
+struct Position {
+  float x;
+  float y;
+  float z;
+};
+
+struct Angles {
+  float phi;
+  float z;
+};
+
+// =======================
+// Global variables
+// =======================
+
+Position antennaPos;
+Position rocketPos;
+
+// =======================
+// Function Declarations
+// =======================
+
+Angles computeAngles(Position, Position);
+Angles radianTodegrees(Angles);
+
+
+// =======================
+// Arduino setup / loop
+// =======================
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Init serial, sensors, motors, etc.
+  // Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+
+  // 1) Read antenna position (encoders)
+
+  
+  // 2) Read rocket position (GPS / telemetry)
+
+
+
+  // 3) Compute target angles
+
+
+
+  // 4) Command motors
+
+
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+Angles computeAngles(Position aPos, Position rPos){
+
+  // Calculations for the line of sight vector Vr - Va
+
+  float dx = rPos.x - aPos.x;
+  float dy = rPos.y - aPos.y;
+  float dz = rPos.z - aPos.z;
+
+  // Compute Angles in radians
+  
+  Angles targetAngles;  
+
+  float horizontalDistance = sqrt( (dx * dx) + (dy * dy) ); // Needed for z angle
+  
+  targetAngles.phi = atan2(dy, dx);
+
+  targetAngles.z = atan2(dz, horizontalDistance);
+
+  return targetAngles;
+}
+
+Angles radianTodegrees(Angles angles){
+
+  Angles degreeAngles;
+
+  degreeAngles.phi = angles.phi * 180 / PI;
+  degreeAngles.z = angles.z * 180 / PI;
+
+  return degreeAngles;
 }
